76e08174863ccd7cc2191353602114337e44ca07
Nick Jarin <email": null>
2017-04-14 15:20:50 -0700

    Add seed file and yaml with list of all congress members

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+congress_members_list = Psych.load_file('db/legislators-current.yaml')
+
+
+congress_members_list.each do |member|
+  CongressMember.create(name: member["name"]["official_full"],
+                        title: member["terms"].last["type"].capitalize,
+                        state: member["terms"].last["state"],
+                        district: member["terms"].last["district"],
+                        website: member["terms"].last["url"],
+                        end: member["terms"].last["end"]
+    )
+end
