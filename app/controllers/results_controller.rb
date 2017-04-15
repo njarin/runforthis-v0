4e665af5f6d3fd1631671f8a8abd004284aa798a
Nick Jarin <email": null>
2017-04-14 21:03:14 -0700

    Add info to controller route for results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,12 +1,18 @@
 require 'geocodio'
 
+get '/results' do
+  "Hello World"
+end
+
 post '/results' do
   geocodio = Geocodio::Client.new(ENV["GEOCODIO_API_KEY"])
   input_address = address_to_string(params[:address])
-  address = geocodio.geocode([input_address], fields: %w[cd stateleg school timezone]).best
-  p "=================================="
-  p address
-  p "=================================="
-  p address.congressional_district
-  p "=================================="
+  @address = geocodio.geocode([input_address], fields: %w[cd stateleg school timezone]).best
+  user_district = @address.congressional_district.district_number
+  user_state = @address.state
+
+  @senators = CongressMember.where(state: user_state, district: nil)
+  @representatives = CongressMember.where(state: user_state, district: user_district)
+
+  erb :'results'
 end
